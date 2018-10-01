0fc8b1327571db7a0853795acc511a9131b8877f
Antoine Fabri <antoine.fabri@gmail.com>
2018-10-01 20:15:04 +0200

    signif2 renamed as smart_signif

--- R/smart_signif.R
+++ R/smart_signif.R
@@ -13,6 +13,7 @@
 #'
 #' @param x numeric, length 2 or more
 #' @param digits integer, number of preferred remaining significant digits
+#' @param ... unused, kept for compatibility reasons
 #' @return numeric vector
 #' @export
 #' @md
@@ -20,15 +21,11 @@
 #' \dontrun{
 #' set.seed(1)
 #' x  <- cumsum(abs(rnorm(10,100,100)))
-#' # [1]   37.35462  155.71895  172.15609  431.68417  564.63495  582.58811  731.33101  905.16348 1062.74162 1132.20278
-#' signif2(x, 1)
-#' #  [1]   40  160  170  400  560  580  700  900 1060 1130
-#' signif2(x, 2)
-#' #  [1]   37  160  170  430  560  580  730  910 1060 1130
-#' signif2(x, 3)
-#' #  [1]   37.4  156.0  172.0  432.0  565.0  583.0  731.0  905.0 1060.0 1130.0
+#' smart_signif(x, 1)
+#' smart_signif(x, 2)
+#' smart_signif(x, 3)
 #' }
-signif2 <- function(x, digits=1L) {
+smart_signif <- function(x, digits=1L, ...) {
   stopifnot(length(x) > 1L)
   logscale <- ceiling(log10(abs(x)))
   logdiff <- log10(diff(x))
