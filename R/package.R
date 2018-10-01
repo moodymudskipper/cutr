b959d3a1ed0fee6f5b38632eae79210cc23ca41f
Antoine Fabri <antoine.fabri@gmail.com>
2018-10-01 20:15:53 +0200

    package description

---
+++ R/package.R
@@ -0,0 +1,8 @@
+#' cutf: Enhanced cut And Useful Related Functions
+#'
+#' Expands formatting options of `base::cut.default` and `Hmisc::cut2`,
+#' supports factors, adhoc formating, group optimization and much more.
+#'
+#' @docType package
+#' @name cutf
+NULL
